3a9002657b31fe7b7ef20b92252979855c40dbf2
Nick Celaya <nickcelaya07@gmail.com>
2020-09-22 11:18:57 -0600

    Added onInputChange function the update state on user input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-	state = {};
+	state = {
+		userInput: 'static state input',
+	};
+
+	onInputChange = (event) => {
+		this.setState({
+			userInput: event.target.value,
+		});
+	};
+
 	render() {
+		console.log(this.state.userInput, 'state');
 		return (
 			<div className='ui segment'>
 				<form className='ui form'>
 					<label>Image Search</label>
-					<input className='field' type='text' />
+					<input
+						className='field'
+						type='text'
+						value={this.state.userInput}
+						onChange={this.onInputChange}
+					/>
 				</form>
+				{this.state.userInput}
 			</div>
 		);
 	}
