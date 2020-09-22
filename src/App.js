16892e1c7656cb212afa4a44e5c5cf40147b87e4
Nick Celaya <nickcelaya07@gmail.com>
2020-09-22 15:51:14 -0600

    Added axios to the app and reached out to unplash api for data with get request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import SearchBar from './components/SearchBar';
 import './App.css';
 
 class App extends Component {
+	//?query={userInput}
 	//function passed down to SearchBar.js through props to get user input back from SearchBar.js
 	onSearchSubmit = (userInput) => {
+		axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: userInput },
+			headers: {
+				Authorization: 'Client-ID 9bDnVwHCHRLoOVnjHjUK-zKCwiUezalAWvyvQVv94pE',
+			},
+		});
 		console.log(userInput);
 	};
 	render() {
@@ -16,4 +24,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default App; 
