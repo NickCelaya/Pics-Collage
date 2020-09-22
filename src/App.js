ed506af8b187231f791a91a11ae624c5cfe84d23
Nick Celaya <nickcelaya07@gmail.com>
2020-09-22 14:06:21 -0600

    Added helper function in app.js to take user input from SearchBar.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 import './App.css';
 
-const App = () => {
-	return (
-		<div className='ui container' style={{ marginTop: '10px' }}>
-			<SearchBar />
-		</div>
-	);
-};
+class App extends Component {
+	//function passed down to SearchBar.js through props to get user input back from SearchBar.js
+	onSearchSubmit = (userInput) => {
+		console.log(userInput);
+	};
+	render() {
+		return (
+			<div className='ui container' style={{ marginTop: '10px' }}>
+				<SearchBar onSubmit={this.onSearchSubmit} />
+			</div>
+		);
+	}
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,19 +2,28 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 	state = {
-		userInput: 'static state input',
+		userInput: '',
 	};
 
+	//Captures the users input and is called on the input jsx element
 	onInputChange = (event) => {
 		this.setState({
 			userInput: event.target.value,
 		});
 	};
 
+	onFormSubmit = (event) => {
+		//prevents default submit
+		event.preventDefault();
+
+		//passes the users input from state back to the parent component App.js through onSubmit function call on props
+		console.log(this.props.onSubmit(this.state.userInput));
+	};
+
 	render() {
 		console.log(this.state.userInput, 'state');
 		return (
-			<div className='ui segment'>
+			<div className='ui segment' onSubmit={this.onFormSubmit}>
 				<form className='ui form'>
 					<label>Image Search</label>
 					<input
